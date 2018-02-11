6d4ab69db56e1fbdebcfbb6f862d5d893c21d205
Pfollvosten <lukas.boehm00@gmx.de>
2018-02-11 19:56:57 +0100

    first scripts etc (init 2)

---
+++ VoxelRunner/Assets/Scripts/InputManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour {
+
+    public static bool jump = false;
+    public static float speed = 0f;
+
+	void Update () {
+        if (Input.GetButtonUp("Jump"))
+        {
+            GetComponent<Animator>().SetTrigger("jump");
+        }
+        speed = Mathf.Abs(Input.GetAxis("Vertical"));
+	}
+}
---
+++ VoxelRunner/Assets/Scripts/MovementController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour {
+
+    private float speed; //range 0;1
+    private Animator anim;
+    private CharacterController charcon;
+    public float speedmul;
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+        charcon = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        speed = InputManager.speed;
+        charcon.Move(new Vector3(0, 0,speed/speedmul));
+        anim.SetFloat("speed", speed);
+
+	}
+}
